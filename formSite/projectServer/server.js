17f79ce57d3227b6d66fb221d0ae081b30df58b3
Rithsagea Aquadom <rithsagea@gmail.com>
2022-11-05 16:43:10 -0400

    base form

--- formSite/projectServer/server.js
+++ formSite/projectServer/server.js
@@ -1,9 +1,21 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const requestListener = function (req, res) {
-	res.writeHead(200);
-	res.end('Hello, World');
-};
+const app = express();
+const port = 8080;
 
-const server = http.createServer(requestListener);
-server.listen(8080);
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/api/form', (req, res) => {
+	const form = req.body;
+
+	console.log(form);
+	res.send('Received form!');
+});
+
+app.listen(port, () => {
+	console.log(`Makerspace server listening on port ${port}`);
+});
