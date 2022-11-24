5c03f6dd6a0dd1e3ff278c602bb990fa96b4cf72
Rithsagea Aquadom <rithsagea@gmail.com>
2022-11-24 17:44:06 -0500

    move to ejs

--- server.js
+++ server.js
@@ -9,21 +9,39 @@ const client = new MongoClient(db_url);
 const db = client.db('makerspace');
 const projects = db.collection('projects');
 
+async function addProject(project) {
+	const res = await projects.insertOne(project);
+}
+
 const app = express();
-const port = 8080;
+const port = 8000;
 const upload = multer();
 
 app.use(cors());
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+	res.render('pages/index');
+});
 
 app.get('/form', (req, res) => {
-	res.sendFile(path.join(__dirname, '/form.html'));
+	res.render('pages/form');
 });
 
 app.post('/api/form', upload.any(), (req, res) => {
 	console.log(req.body); // form info
 	console.log(req.files); // form files
 	res.send('Received form!');
+
+	const project = {
+		name: 'Lower GI',
+		category: 'Science',
+		attributions: ['cc', 'sharealike'],
+		designer: 'https://www.thingiverse.com/airforce/designs'
+	};
+
+	addProject(project);
 });
 
 app.listen(port, () => {
